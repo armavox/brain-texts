9eba2db143c9f86f71a93ba3cf192afe501154d5
Artem Lobantsev <armavox@gmail.com>
2019-06-12 07:50:20 +0000

    Script updated

--- segmentation/train_seg.sh
+++ segmentation/train_seg.sh
@@ -2,6 +2,6 @@ python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brai
 python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.01
 python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.01 -d 0.3
 python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001
-python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001 -bs 2
+python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001 -bs 8
 python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001 -d 0.3
-python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001 -d 0.3 -bs 2
+python train/trainTorch.py -i /data/brain/rs-mhd-dataset-augmented -c /data/brain/checkpoints -e 15 -l 0.0001 -d 0.3 -bs 8
